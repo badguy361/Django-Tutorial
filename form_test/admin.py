a9e8251947fd471806aa5a6084e86940c6dbd1d7
joey <t1616joy@yahoo.com.tw>
2023-02-17 23:45:24 +0800

    admin add search bar

--- form_test/admin.py
+++ form_test/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from form_test.models import Restaurant, Food
 
-admin.site.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin): # list_display search_fields為固定用法
+    list_display = ('name', 'phone_number', 'address')
+    search_fields = ('name',) 
+
+admin.site.register(Restaurant, RestaurantAdmin)
 admin.site.register(Food)
