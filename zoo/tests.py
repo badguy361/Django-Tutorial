bea6acfd49f0552ba4ed6a1ce88a57e123702b68
joey <t1616joy@yahoo.com.tw>
2023-03-14 20:06:31 +0800

    run test

--- zoo/tests.py
+++ zoo/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.test import client
+from zoo import models
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+    def test_dog_says(self):
+        dog = models.Dog(name="Snoopy")
+        self.assertEqual(dog.says(), 'woof')
+
+    def test_cat_says(self):
+        cat = models.Cat(name="Garfield")
+        self.assertEqual(cat.says(), 'meow')
+
+class IndexWebpageTestCase(TestCase):
+
+    def setUp(self):
+        self.c = client.Client()
+
+    def test_add_visiting(self):
+        resp = self.c.get('/1/plus/2/')
+        self.assertEqual(resp.status_code, 200)
+        # self.assertContains(resp, '<p>歡迎來到餐廳王</p>')
+        self.assertTemplateUsed(resp, 'main.html')
